a86b3559204055fcebb969f41820a5cd852a59cd
hybras <24651269+hybras@users.noreply.github.com>
2022-04-10 07:17:37 -0400

    Disable street view

--- src/Picker.tsx
+++ src/Picker.tsx
@@ -52,7 +52,8 @@ export default class Picker extends React.Component<{ img_id: string }, {
                     lat: umd.lat,
                     lng: umd.lng,
                 },
-                zoom: 15 // You can set any google map options here, zoom defaults to 15
+                zoom: 15, // You can set any google map options here, zoom defaults to 15
+                streetViewControl: false
             }),
             loc: umd
         })
