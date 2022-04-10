817c9f820579e16d93d385b9cd3ce2224efa0ae9
hybras <24651269+hybras@users.noreply.github.com>
2022-04-09 21:33:46 -0400

    Rename class to className

--- src/Landing.js
+++ src/Landing.js
@@ -2,13 +2,13 @@ import './Landing.css';
 
 export default function Landing() {
     return (<div>
-        <div class="topbar">
-            <a class="button" href="blank.html">ABOUT</a>
-            <a class="button" href="blank.html">CONTACT</a>
+        <div className="topbar">
+            <a className="button" href="blank.html">ABOUT</a>
+            <a className="button" href="blank.html">CONTACT</a>
         </div>
-        <div class="container">
-            <img name="backgr" class="bgimg" src=".\photos\0.jpg" alt="Blurry scene of unknown umd scene" />
-            <button class="play">PLAY!</button>
+        <div className="container">
+            <img name="backgr" className="bgimg" src=".\photos\0.jpg" alt="Blurry scene of unknown umd scene" />
+            <button className="play">PLAY!</button>
         </div>
     </div>)
 }
