0bfd211b211347112d3643d0975e9b66b761c3b2
hybras <24651269+hybras@users.noreply.github.com>
2022-04-10 08:52:06 -0400

    Remove location picker middle man

--- src/Picker.tsx
+++ src/Picker.tsx
@@ -1,14 +1,15 @@
 import './Picker.css';
-import LocationPicker from "location-picker";
+// import LocationPicker from "location-picker";
 import React from 'react';
 
 const umd = { lat: 38.98706766398821, lng: - 76.94259906224374 }
 const config_path = "config/";
-const convertPoint = ({ x, y }) => ({ lat: y, lng: x })
 
+// this is intentionally flipped
+const convertPoint = ({ x, y }: { x: number, y: number }) => { return { lat: x, lng: y } }
 
 export default class Picker extends React.Component<{ img_id: string }, {
-    inner: LocationPicker, loc: {
+    inner: any, loc: {
         lat: number;
         lng: number;
     }, ans: {
@@ -23,6 +24,7 @@ export default class Picker extends React.Component<{ img_id: string }, {
             .then(data =>
                 data.json()
             ).then(data => {
+                console.log(`ans: (${data.loc.lat}, ${data.loc.lng})`);
                 this.state = {
                     loc: umd,
                     inner: null!!,
@@ -35,32 +37,34 @@ export default class Picker extends React.Component<{ img_id: string }, {
         return (<>
             <div id="picker" className="Picker" />
             <button className="square" onClick={() => {
-                this.setState({ loc: this.state.inner.getMarkerPosition() })
+                // this.setState({ loc: this.state.inner.getMarkerPosition() })
                 // alert(`${this.state.loc.lat}, ${this.state.loc.lng}`)
-                const dist = google.maps.geometry.spherical.computeDistanceBetween(this.state.ans, this.state.loc)
+                console.log(`loc: (${this.state.loc.lat}, ${this.state.loc.lng})`)
+                const dist = google.maps.geometry.spherical.computeDistanceBetween(umd, this.state.loc)
                 alert(dist)
             }}>Confirm Position</button>
         </>);
     }
 
     componentDidMount() {
-        const inner = new LocationPicker('picker', {
-            setCurrentPosition: false,
-        }, {
-            center: {
-                lat: umd.lat,
-                lng: umd.lng,
-            },
-            zoom: 15, // You can set any google map options here, zoom defaults to 15
-            streetViewControl: false
+        console.log(`umd:(${umd.lat}, ${umd.lng})`);
+        const inner = new google.maps.Map(document.getElementById("picker"), {
+            zoom: 15,
+            center: umd,
+            streetViewControl: false,
         });
-        if (inner === null) {
-            console.log("inner init as null");
-        } else {
+        this.setState({
+            inner: inner,
+        })
+        inner.addListener("click", (mapsMouseEvent) => {
+            console.log(`click: (${mapsMouseEvent.latLng.lat()} ${mapsMouseEvent.latLng.lng()})`)
             this.setState({
-                inner: inner,
-                loc: umd
+                loc: {
+                    lat: mapsMouseEvent.latLng.lat(),
+                    lng: mapsMouseEvent.latLng.lng(),
+                },
             })
-        }
+        });
+
     }
 }
