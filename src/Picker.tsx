34d2d736109977eaf0e490add95d606447604f70
hybras <24651269+hybras@users.noreply.github.com>
2022-04-10 08:58:34 -0400

    Move constructor logic into did mount

--- src/Picker.tsx
+++ src/Picker.tsx
@@ -17,22 +17,6 @@ export default class Picker extends React.Component<{ img_id: string }, {
         lng: number;
     }
 }> {
-    constructor(props) {
-        super(props)
-        fetch(`/json/${props.img_id
-            }.json`)
-            .then(data =>
-                data.json()
-            ).then(data => {
-                console.log(`ans: (${data.loc.lat}, ${data.loc.lng})`);
-                this.state = {
-                    loc: umd,
-                    inner: null!!,
-                    ans: convertPoint(data.loc)
-                }
-            })
-    }
-
     render(): React.ReactNode {
         return (<>
             <div id="picker" className="Picker" />
@@ -40,21 +24,27 @@ export default class Picker extends React.Component<{ img_id: string }, {
                 // this.setState({ loc: this.state.inner.getMarkerPosition() })
                 // alert(`${this.state.loc.lat}, ${this.state.loc.lng}`)
                 console.log(`loc: (${this.state.loc.lat}, ${this.state.loc.lng})`)
-                const dist = google.maps.geometry.spherical.computeDistanceBetween(umd, this.state.loc)
-                alert(dist)
+                const dist = google.maps.geometry.spherical.computeDistanceBetween(this.state.ans, this.state.loc)
             }}>Confirm Position</button>
         </>);
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         console.log(`umd:(${umd.lat}, ${umd.lng})`);
         const inner = new google.maps.Map(document.getElementById("picker"), {
             zoom: 15,
             center: umd,
             streetViewControl: false,
         });
+        const fetched = await fetch(`/json/${this.props.img_id
+            }.json`)
+        const data = await fetched.json()
+        console.log(`ans: (${data.loc.lat}, ${data.loc.lng})`);
+
         this.setState({
             inner: inner,
+            ans: convertPoint(data.loc),
+            loc: umd,
         })
         inner.addListener("click", (mapsMouseEvent) => {
             console.log(`click: (${mapsMouseEvent.latLng.lat()} ${mapsMouseEvent.latLng.lng()})`)
