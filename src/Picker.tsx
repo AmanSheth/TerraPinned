ecca2df6007dd49fd494cd86ea013f93d4d44c75
hybras <24651269+hybras@users.noreply.github.com>
2022-04-10 05:56:27 -0400

    Try to get img ans using id

--- src/Picker.tsx
+++ src/Picker.tsx
@@ -3,24 +3,38 @@ import LocationPicker from "location-picker";
 import React from 'react';
 
 const umd = { lat: 38.98706766398821, lng: - 76.94259906224374 }
+const config_path = "config/";
+const convertPoint = ({ x, y }) => ({ lat: y, lng: x })
 
-export default class Picker extends React.Component<{}, {
+
+export default class Picker extends React.Component<{ id: string }, {
     inner: LocationPicker, loc: {
+        lat: number;
+        lng: number;
+    }, ans: {
         lat: number;
         lng: number;
     }
 }> {
     constructor(props) {
         super(props)
-        this.state = {
-            loc: umd,
-            inner: null!!
-        }
+        fetch(`/config/${props.id}.json`)
+            .then(data => {
+                data.json()
+            }).then(data => {
+                this.state = {
+                    loc: umd,
+                    inner: null!!,
+                    ans: convertPoint(data.loc)
+                }
+            })
     }
 
     onSubmit() {
         this.setState({ loc: this.state.inner.getMarkerPosition() })
-        alert(`${this.state.loc.lat}, ${this.state.loc.lng}`)
+        // alert(`${this.state.loc.lat}, ${this.state.loc.lng}`)
+        const dist = google.maps.geometry.spherical.computeDistanceBetween(this.state.ans, this.state.loc)
+        alert(dist)
     }
 
     render(): React.ReactNode {
